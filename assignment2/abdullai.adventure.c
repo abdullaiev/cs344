90b86fe62b0e731e6c4b3a2d1c75abb46c283d7d
illia-osu <abdullai@oregonstate.edu>
2018-02-12 21:33:27 -0800

    Update timekeeping to use multi-threads.

--- assignment2/abdullai.adventure.c
+++ assignment2/abdullai.adventure.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <time.h>
+#include <pthread.h>
 
 struct room {
     char name[256];
@@ -26,12 +27,16 @@ int getMostRecentDirectoryName(char name[]) {
         return 1;
     }
 
+    //Loop over all the folders in the current directory.
     while ((dir = readdir(currentDirectory)) != NULL) {
         //If it is a directory that has "abdullai.rooms." in its name.
         if (dir->d_type == DT_DIR && strstr(dir->d_name, "abdullai.rooms.") != NULL) {
+            //Get the stats on this directory.
             stat(dir->d_name, &directoryStat);
 
+            //Compare if this one is the most recently updated one.
             if (directoryStat.st_mtime > timestamp) {
+                //If it is, save its name.
                 timestamp = directoryStat.st_mtime;
                 snprintf(name, 256, "%s", dir->d_name);
             }
@@ -51,6 +56,7 @@ int readByLine(char fileName[], struct room rooms[], int roomIndex) {
     size_t len = 0;
     char *token;
 
+    //Foe every line in the room file.
     while (getline(&line, &len, filePointer) != -1) {
         if (lineCount == 0) {
             //Save room name if this is the first line of the file.
@@ -107,6 +113,8 @@ int parseRoomFiles(char directory[], struct room rooms[]) {
     }
 
     int count = 0;
+
+    //For every room file in the given directory.
     while ((file = readdir(roomFilesDirectory)) != NULL) {
         char filePath[256];
 
@@ -115,6 +123,7 @@ int parseRoomFiles(char directory[], struct room rooms[]) {
             continue;
         }
 
+        //Get the file name and parse each line of this file with readByLine function.
         snprintf(filePath, sizeof filePath, "%s/%s", directory, file->d_name);
         readByLine(filePath, rooms, count);
         count++;
@@ -126,16 +135,19 @@ int parseRoomFiles(char directory[], struct room rooms[]) {
 
 //Finds the room by given query and type criteria.
 int findRoom(struct room rooms[], char query[], char type[]) {
-    int i = 0;
+    int i;
     int length = 7;
 
-    for (; i < length; i++) {
+    //Loop over all the rooms.
+    for (i = 0; i < length; i++) {
         if (strcmp(type, "name") == 0) {
             if (strcmp(rooms[i].name, query) == 0) {
+                //Found the room with name == query.
                 return i;
             }
         } else if (strcmp(type, "type") == 0) {
             if (strcmp(rooms[i].type, query) == 0) {
+                //Found the room with type == query.
                 return i;
             }
         }
@@ -146,6 +158,8 @@ int findRoom(struct room rooms[], char query[], char type[]) {
 
 //Prints out current possible connections to other room and stores user's input in the passed input string.
 void askForInput(struct room rooms[], int currentRoom, char input[], int askedTime) {
+    //In regular mode, game outputs current location and possible connections.
+    //If user asked for current time, the game will output current time and then skip current location and possible connections.
     if (askedTime == 0) {
         printf("CURRENT LOCATION: %s\n", rooms[currentRoom].name);
         printf("POSSIBLE CONNECTIONS: ");
@@ -169,6 +183,7 @@ void askForInput(struct room rooms[], int currentRoom, char input[], int askedTi
     if (input[len] == '\n') {
         input[len] = '\0';
     }
+
     printf("\n");
 }
 
@@ -191,14 +206,21 @@ void congratulateUser(char path[][256], int steps) {
     printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", steps);
 
     int i = 0;
+    //Print out every room that the user has visited.
     while (path[i][0] != '*') {
         printf("%s\n", path[i]);
         i++;
     }
 }
 
-//Gets current user's time , formats it as "1:03pm, Tuesday, September 13, 2016" and stores into file currentTime.txt.
-void writeCurrentTime() {
+
+//Gets current user's time, formats it as "1:03pm, Tuesday, September 13, 2016" and stores into file currentTime.txt.
+//This functions is executed in a separate thread. See startTimeThread for reference.
+void* writeCurrentTime(void* arg) {
+    //Acquire a mutex look for this thread so no other threads can lock it.
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_lock(&mutex);
+
     char timeStr[256];
     struct tm *tmStruct;
     time_t currentTime;
@@ -210,6 +232,17 @@ void writeCurrentTime() {
     FILE *filePointer = fopen("currentTime.txt", "w");
     fprintf(filePointer, "%s", timeStr);
     fclose(filePointer);
+
+    //Current time has been stored in a file, the mutex lock can be released now.
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+//Starts a separate thread to write current time into a file.
+void startTimeThread() {
+    pthread_t timeThreadID;
+    pthread_create(&timeThreadID, NULL, writeCurrentTime, NULL);
+    pthread_join(timeThreadID, NULL);
 }
 
 //Reads time stored in currentTime.txt and prints it to the console.
@@ -217,6 +250,7 @@ void readCurrentTime() {
     char *line = NULL;
     size_t len = 0;
     FILE *filePointer = fopen("currentTime.txt", "r");
+    //There will be just one line with time details. No need to loop over file contents.
     getline(&line, &len, filePointer);
     printf("%s\n\n", line);
     fclose(filePointer);
@@ -225,13 +259,13 @@ void readCurrentTime() {
 //Starts asking the user for input and ends the game once the end-room is reached.
 void startGame(struct room rooms[]) {
     int steps = 0;
-    int currentRoom = findRoom(rooms, "START_ROOM", "type");
-    int endRoom = findRoom(rooms, "END_ROOM", "type");
-    char path[1000][256];
-    char input[256];
-    //When set to 1, current room and possible connections will not be logged out.
-    int askedTime = 0;
+    int currentRoom = findRoom(rooms, "START_ROOM", "type"); //Begin from start room.
+    int endRoom = findRoom(rooms, "END_ROOM", "type"); //Store end room index for later usage.
+    char path[100][256]; //An array to store all rooms visited.
+    char input[256]; //User's input.
+    int askedTime = 0; //When set to 1, current room and possible connections will not be logged out.
 
+    //Keep asking for room to enter until end room is reached.
     while (1) {
         askForInput(rooms, currentRoom, input, askedTime);
         askedTime = 0;
@@ -239,7 +273,8 @@ void startGame(struct room rooms[]) {
         //Check if the user wishes to see current time.
         if (strcmp(input, "time") == 0) {
             askedTime = 1;
-            writeCurrentTime();
+            //Store time in a separate thread. That thread will use mutexes. See writeCurrentTime();
+            startTimeThread();
             readCurrentTime();
             continue;
         }
